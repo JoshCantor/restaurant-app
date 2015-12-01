36ecc19cd4815731cdfd029c7317f2ca47a1cb01
JoshCantor <josh.s.cantor@gmail.com>
2015-11-30 16:17:59 -0800

    menu api fetches menu items and displays them

---
+++ javascript/form.js
@@ -0,0 +1,48 @@
+var isNotIncluded = function(el, arr) {
+	for (i in arr) {
+		var currentEl = arr[i];
+		if (el === currentEl) {
+			return false;
+		}
+	}
+	return true;
+}
+
+
+var getMenu = function() {
+	var apiRequest = $.ajax({
+	  	url: 'https://galvanize-eats-api.herokuapp.com/menu',
+	  	method: "GET",
+	});
+
+	apiRequest.done(function(data) {
+		var menu = data.menu;
+		var itemTypes = [];
+		
+		for (i in menu) {
+			var item = menu[i];
+			var optGroup = item.type;
+			if (isNotIncluded(optGroup, itemTypes)) {
+				itemTypes.push(optGroup);
+			}
+		}
+
+		for (i in itemTypes) {
+			var type = itemTypes[i];
+			var optGroup = $('<optGroup>').addClass(type).attr('label', type);
+			$('select').append(optGroup);
+		}
+
+		for (i in menu) {
+			var item = menu[i];
+			console.log(item);
+			var option = $('<option>').html(item.name, item.price);
+			var type = $('.' + item.type);
+			type.append(option);
+		}
+
+	})
+}
+
+getMenu();
+
